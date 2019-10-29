092dcb6fa1a4062eea04e5414ca68277fc4e4382
nvarg <natvargas97@gmail.com>
2019-10-29 14:24:00 -0700

    Fixed pipeline_stream.sh. bonus: now parallel

--- pipeline_stream.sh
+++ pipeline_stream.sh
@@ -1,6 +1,22 @@
-cat "$(dirname $0)/sublist" \
-    | xargs -n1 -I'{}' "$(dirname $0)/scripts/get_posts_urls.sh" '{}' -s hot \
-    | xargs -n1 "$(dirname $0)/scripts/get_comments.sh" \
-    | jq --stream -nrfc "$(dirname $0)/scripts/flatten_comment_lists_streaming.jq" \
-    > "$(dirname $0)/data/$(date +%m_%d_%Y.json)"
+#!/usr/bin/env sh
+
+tmpdir=$(mktemp -t reddit.XXXXX)
+trap "rm $tmpdir -R" INT QUIT TERM EXIT
+
+while read subreddit; do
+    echo "Reading posts from /$subreddit."
+    for post in $($(dirname $0)/scripts/get_posts_urls.sh $subreddit -s hot); do
+        ($(dirname $0)/scripts/get_comments.sh $post \
+            > $tmpdir/$(basename $post) &)
+    done
+done < "$(dirname $0)/sublist"
+echo "Working..."
+wait
+
+for tmp in $tmpdir/*; do
+    jq --stream -nrfc \
+        "$(dirname $0)/scripts/flatten_comment_lists_streaming.jq" "$tmp" \
+        >> "$(dirname $0)/data/$(date +%m_%d_%Y_test.json)"
+done
+echo "Done."
 
