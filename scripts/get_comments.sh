0fc066ddacad41921f0f09d4c3b77c25ad465925
nvarg <natvargas97@gmail.com>
2019-10-29 17:16:42 -0700

    compact jq output

--- scripts/get_comments.sh
+++ scripts/get_comments.sh
@@ -13,5 +13,5 @@ requires curl >/dev/null
 requires jq >/dev/null
 echo $1 | grep -E '/r/\w+/comments/\w+/\w+/' >/dev/null || usage
 
-curl -s -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0" "https://www.reddit.com$1.json" | jq -r '.[1] | .data.children[]'
+curl -s -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0" "https://www.reddit.com$1.json" | jq -rc '.[1] | .data.children[]'
 
